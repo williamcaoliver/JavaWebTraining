e25e73b9a61532014afe18b130f868aeab248648
William Oliver <woliver@scottlogic.com>
2023-01-11 16:15:53 +0000

    WIP: Basic Matcher and Test Objs

--- WebApp/src/main/java/springboot/WebApp/WebAppApplication.java
+++ WebApp/src/main/java/springboot/WebApp/WebAppApplication.java
@@ -2,12 +2,38 @@ package springboot.WebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springboot.WebApp.dao.Enums;
+import springboot.WebApp.dao.Order;
+import springboot.WebApp.services.Matcher;
 
 @SpringBootApplication
 public class WebAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebAppApplication.class, args);
+		//SpringApplication.run(WebAppApplication.class, args);
+		Matcher matcher = new Matcher();
+		Order newOrder = new Order("James", 10, 15, Enums.TradeActions.SELL);
+		Order newOrder1 = new Order("Alice", 50, 25, Enums.TradeActions.BUY);
+		Order newOrder2 = new Order("Joe", 100, 10, Enums.TradeActions.SELL);
+		Order newOrder3 = new Order("Will", 100, 10, Enums.TradeActions.BUY);
+		Order newOrder4 = new Order("Ronnie", 100, 10, Enums.TradeActions.SELL);
+		Order newOrder5 = new Order("James", 25, 50, Enums.TradeActions.BUY);
+		Order newOrder6 = new Order("Joe", 15, 10, Enums.TradeActions.BUY);
+		Order newOrder7 = new Order("Seb", 50, 50, Enums.TradeActions.BUY);
+		Order newOrder8 = new Order("Adam", 30, 15, Enums.TradeActions.SELL);
+
+		matcher.addNewOrder(newOrder);
+		matcher.addNewOrder(newOrder1);
+		matcher.addNewOrder(newOrder2);
+		matcher.addNewOrder(newOrder3);
+		matcher.addNewOrder(newOrder4);
+		matcher.addNewOrder(newOrder5);
+		matcher.addNewOrder(newOrder6);
+		matcher.addNewOrder(newOrder7);
+		matcher.addNewOrder(newOrder8);
 	}
+
+
+
 
 }
--- WebApp/src/main/java/springboot/WebApp/services/Matcher.java
+++ WebApp/src/main/java/springboot/WebApp/services/Matcher.java
@@ -1,4 +1,113 @@
 package springboot.WebApp.services;
 
+import springboot.WebApp.dao.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Matcher {
+    private ArrayList<Order> buyOrders = new ArrayList<Order>();
+    private ArrayList<Order> sellOrders = new ArrayList<Order>();
+    private ArrayList<Trade> tradesList = new ArrayList<Trade>();
+    private ArrayList<OrderBookEntry> aggOrderBook = new ArrayList<OrderBookEntry>();
+
+    public void addNewOrder(Order newOrder) {
+        if (validateOrder(newOrder)) {
+            System.out.println("Success");
+            if (newOrder.getAction() == Enums.TradeActions.BUY) {
+                buyOrders.add(newOrder);
+                System.out.println("Buy Order added to Buy Array");
+                buyOrders.forEach(Order::showData);
+                Collections.sort(buyOrders, new OrderArraySortDesc());
+                System.out.println("Sorted");
+                buyOrders.forEach(Order::showData);
+            } else if (newOrder.getAction() == Enums.TradeActions.SELL) {
+                sellOrders.add(newOrder);
+                System.out.println("Sell Order added to Sell Array");
+                sellOrders.forEach(Order::showData);
+                Collections.sort(sellOrders, new OrderArraySortAsc());
+                System.out.println("Sorted");
+                sellOrders.forEach(Order::showData);
+
+            }
+            matchOrders(newOrder);
+
+        } else {
+            System.out.println("Fail");
+        }
+    }
+
+    private boolean validateOrder(Order newOrder) {
+        if (newOrder == null) {
+            System.out.println("The new order is Null");
+            return false;
+        }
+//      for (Enums.TradeActions action : Enums.TradeActions.values()){
+//
+//            if(!(action.equals(newOrder.getAction()))){
+//                System.out.println("Order action is invalid");
+//            }
+//        }
+        if (newOrder.getAction() == null) {
+            System.out.println("No Order Action: " + newOrder.getAction());
+            return false;
+        }
+        if (newOrder.getPrice() <= 0) {
+            System.out.println("Price below 0 :" + newOrder.getPrice());
+            return false;
+        }
+        if (newOrder.getQuantity() <= 0) {
+            System.out.println("Quantity below 0 :" + newOrder.getQuantity());
+            return false;
+        }
+
+        return true;
+
+    }
+
+    private void matchOrders(Order newOrder) {
+        if (newOrder.getAction() == Enums.TradeActions.BUY && sellOrders.size() != 0) {
+            matchBuyer(newOrder);
+            tradesList.forEach(Trade::showData);
+            System.out.println("End");
+        } else if (newOrder.getAction() == Enums.TradeActions.SELL && buyOrders.size() != 0) {
+            matchSeller(newOrder);
+            tradesList.forEach(Trade::showData);
+            System.out.println("End");
+        } else {
+            System.out.println("No match");
+        }
+    }
+
+    private void matchBuyer(Order newOrder) {
+        for (int i = 0; i < sellOrders.size(); i++) {
+            Order currentSellOrder = sellOrders.get(i);
+            if (newOrder.getPrice() >= currentSellOrder.getPrice() && newOrder.getQuantity() != 0) {
+                Trade tradedObj = new Trade(newOrder, currentSellOrder, newOrder.getPrice(), Math.min(newOrder.getQuantity(), currentSellOrder.getQuantity()));
+                tradesList.add(tradedObj);
+                System.out.println("Matched BUY");
+                System.out.println("Matching End");
+
+            }
+
+
+        }
+    }
+
+    private void matchSeller(Order newOrder) {
+        for (int i = 0; i < buyOrders.size(); i++) {
+            Order currentBuyOrder = buyOrders.get(i);
+            if (newOrder.getPrice() >= currentBuyOrder.getPrice() && newOrder.getQuantity() != 0) {
+                Trade tradedObj = new Trade(currentBuyOrder, newOrder, currentBuyOrder.getPrice(), Math.min(newOrder.getQuantity(), currentBuyOrder.getQuantity()));
+                tradesList.add(tradedObj);
+                System.out.println("Matched Sell");
+                System.out.println("Matching End");
+
+            }
+
+
+        }
+    }
+
+
 }
