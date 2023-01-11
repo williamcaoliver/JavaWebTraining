ec7a8ef47689a80b08680fc1ae1609707dfddbd9
William Oliver <woliver@scottlogic.com>
2023-01-11 16:15:25 +0000

    WIP: Created Sorting Functionality

---
+++ WebApp/src/main/java/springboot/WebApp/services/OrderArraySortAsc.java
@@ -0,0 +1,14 @@
+package springboot.WebApp.services;
+import springboot.WebApp.dao.Order;
+import java.util.Comparator;
+
+public class OrderArraySortAsc implements Comparator<Order> {
+
+    public int compare(Order order1, Order order2){
+        if(order2.getPrice() == order1.getPrice()) {
+            int dateTimeDiff = (int)(order1.getDateTime() - order2.getDateTime());
+            return dateTimeDiff;
+        }
+        return order1.getPrice() - order2.getPrice();
+    }
+}
