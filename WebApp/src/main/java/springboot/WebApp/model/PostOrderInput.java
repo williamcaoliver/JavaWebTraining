c175d527fe0951fcc9938e4ac982fd6d32c86c33
William Oliver <woliver@scottlogic.com>
2023-01-24 15:43:02 +0000

    Validation For Server EndPoint

---
+++ WebApp/src/main/java/springboot/WebApp/model/PostOrderInput.java
@@ -0,0 +1,21 @@
+package springboot.WebApp.model;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import springboot.WebApp.dao.Enums;
+
+@Getter @Setter @ToString @AllArgsConstructor
+public class PostOrderInput {
+    String accountID;
+    @Min(0) @Max(1000)
+    int price;
+    @Min(0) @Max(1000)
+    int quantity;
+    @NotEmpty
+    Enums.TradeActions action;
+}
